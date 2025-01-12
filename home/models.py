7762846dcedf35ff7d0b73034413bf040d64a537
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-01-12 19:59:36 +0530

    Models and Migrations adding Django Day-7

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    # id=models.AutoField()
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField(null=True,blank=True)
+    address=models.TextField(null=True,blank=True)
+    image=models.ImageField()
+    file=models.FileField()
+    
+class Product(models.Model):
+   pass
